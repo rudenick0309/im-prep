146ad656666ee35b6531efb48195d13593f119e9
rudenick0309 <rudenick0309@gmail.com>
2020-03-17 13:13:10 +0900

    math js

--- test-example/math.js
+++ test-example/math.js
@@ -1,6 +1,6 @@
 // Refactor functions below, if you need to
 const add = (num1, num2) => {
-  return num1 - num2;
+  return num1 + num2;
 };
 
 const substract = (num1, num2) => {
@@ -12,6 +12,9 @@ const multiply = (num1, num2) => {
 };
 
 const divide = (num1, num2) => {
+  if (num2 === 0) {
+    return 'cannot divide something with 0';
+  } 
   return num1 / num2;
 };
 
